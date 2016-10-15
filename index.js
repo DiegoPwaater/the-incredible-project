539ce769c28c78b95f4806dc32deb3ea2f7e0095
DiegoPwaater <diego_0203@outlook.com>
2016-10-15 10:07:46 +0800

    Classadoo autocommit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-//get("#logo").css({width:"300px"})
+get("#logo").css({width:"300px"})
 //
-//
-//var blue = true
-//setInterval(function() {
+
+var blue = true
+setInterval(function() {
 //  blue = !blue
 //  
 //  var color = blue ? "blue" : "black"
 //  
-//  get("#logo").css({ border: "20px solid " + randomColor() })
+  get("#logo").css({ border: "20px solid " + randomColor() })
 //}, 20)
--- utils.js
+++ utils.js
@@ -1,42 +1,42 @@
-////
-//// Change the website.
-////
-//function get(query){
-//  return $(query)
-//};
-//function move(query, down, right)
-//{
-//  query.css('top', '+=' + down + 'px' );
-//  query.css('left', '+=' + right + 'px' );
-//};
+
+// Change the website.
 //
-//function goto(query, top, left)
-//{
-//  query.css("top", top);
-//  query.css("left", left);
-//}
+function get(query){
+  return $(query)
+};
+function move(query, down, right)
+{
+  query.css('top', '+=' + down + 'px' );
+  query.css('left', '+=' + right + 'px' );
+};
+
+function goto(query, top, left)
+{
+  query.css("top", top);
+  query.css("left", left);
+}
+
+function rotate(query, degrees)
+{
+  query.css({'transform' : 'rotate('+ degrees +'deg)'});
+}
+
+function where(query)
+{
+  return query.position();
+}
+
+function get(query){return $(query)};
+
 //
-//function rotate(query, degrees)
-//{
-//  query.css({'transform' : 'rotate('+ degrees +'deg)'});
-//}
+// Colors!
 //
-//function where(query)
-//{
-//  return query.position();
-//}
-//
-//function get(query){return $(query)};
-//
-////
-//// Colors!
-////
-//function randomColor()
-//{
-//  var letters = '0123456789ABCDEF';
-//  var color = '#';
-//  for (var i = 0; i < 6; i++ ) {
-//    color += letters[Math.floor(Math.random() * 16)];
-//  }
-//  return color;
-//}
+function randomColor()
+{
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
