e2fb95f2664381d4eb2d011ea583ca1e92280791
DiegoPwaater <diego_0203@outlook.com>
2016-10-15 10:05:14 +0800

    Classadoo autocommit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-get("#logo").css({width:"300px"})
-
-
-var blue = true
-setInterval(function() {
-  blue = !blue
-  
-  var color = blue ? "blue" : "black"
-  
-  get("#logo").css({ border: "20px solid " + randomCol })
-}, 20)
+//get("#logo").css({width:"300px"})
+//
+//
+//var blue = true
+//setInterval(function() {
+//  blue = !blue
+//  
+//  var color = blue ? "blue" : "black"
+//  
+//  get("#logo").css({ border: "20px solid " + randomColor() })
+//}, 20)
