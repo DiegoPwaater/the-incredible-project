690d7cc6d7e72daee717bea0f05e1b6c3bc69b37
DiegoPwaater <diego_0203@outlook.com>
2016-10-15 09:54:49 +0800

    Classadoo autocommit

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ get("#logo").css({width:"300px"})
 var width = 10
 setInterval(function() {
   width = width + 20
-  get("#logo").css({lef:width})
-}, 500)
+  get("#logo").css({left:width, width: width})
+}, 20)
